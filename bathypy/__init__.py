dff5bbf4a20549ea866eb98bfb773783e455ad52
hcwinsemius <h.c.winsemius@gmail.com>
2024-02-13 12:28:07 +0100

    updated links to submodules #1

--- bathypy/__init__.py
+++ bathypy/__init__.py
@@ -1,5 +1,8 @@
 __version__ = "0.1.0"
 
+# ensure to use pygeos
+import os
+os.environ['USE_PYGEOS'] = '0'
 from bathypy import storage
 from bathypy import icesat2
 from bathypy import gww_requests
